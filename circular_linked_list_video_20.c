381d2bce5d3f61fa1109a4a9bfb1357b3a8e9ba8
Atharva <atharvadiwan65@gmail.com>
2025-09-12 19:20:00 +0530

    program added for circular linked list creation

---
+++ circular_linked_list_video_20.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node* next;
+};
+
+void linked_list_traversal(struct node *head) {
+    struct node *current = head;
+    do {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    while(current != head);
+    printf("\n");
+}
+
+// Function to free a circular linked list
+void free_list(struct node* head) {
+    if (head == NULL) return;
+
+    struct node* current = head->next;
+    struct node* temp;
+
+    // Traverse until we reach the head again
+    while (current != head) {
+        temp = current;
+        current = current->next;
+        free(temp);
+    }
+
+    // Finally free the head node
+    free(head);
+}
+
+int main() {
+    struct node *head;
+    struct node *second;
+    struct node *third;
+    head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+
+    head->data = 7;
+    head->next = second;
+
+    second->data = 10;
+    second->next = third;
+
+    third->data = 24;
+    third->next = head;
+
+    printf("Initial traversal\n");
+    linked_list_traversal(head);
+
+    free_list(head);
+    return 0;
+}
